97bbf6e68ace91ae386968fb5eaac9f47544bcdf
Mo-Khaled <hpmohamed287@icloud.com>
2025-08-25 13:08:28 +0300

    reset

--- Terraform/team1/6-main.tf
+++ Terraform/team1/6-main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "wahdan-state-bucket-2"
+    bucket         = "mokhaled-bucket-1284"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-1"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
