4959c0268c6f165f4eb49d4147384f0b8c4c01a8
Mena Mohamed <104127946+menaosman@users.noreply.github.com>
2025-08-28 09:03:59 +0300

    Update 6-main.tf

--- Terraform/team1/6-main.tf
+++ Terraform/team1/6-main.tf
@@ -1,19 +1,32 @@
 terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "mokhaled-bucket-1286"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock" # Table must exist with partition key LockID (String)
     encrypt        = true
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "vpc" {
   source = "../infrastructure-modules/vpc"
 
-  environment      = "dev"
+  environment     = "dev"
   project         = "sprints"
-  private-subnets = ["10.0.128.0/20", "10.0.144.0/20"]
-  public-subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
+  private_subnets = ["10.0.128.0/20", "10.0.144.0/20"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
   azs             = ["us-east-1a", "us-east-1b"]
 }
 
@@ -21,9 +34,10 @@ module "eks" {
   source          = "../infrastructure-modules/eks"
 
   cluster_name    = "sprints-cluster-0"
-  cluster_version = "1.33"
-  vpc_id         = module.vpc.vpc_id
-  subnet_ids     = module.vpc.private_subnet_ids
-  node_groups    = var.node_groups
-  depends_on     = [module.vpc]
+  cluster_version = "1.31"  # 1.31 currently supported by EKS
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnet_ids
+  node_groups     = var.node_groups
+
+  depends_on      = [module.vpc]
 }
